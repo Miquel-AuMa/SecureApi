427cc9076b9e9580359ccd6775317d219e2d0c55
Miquel <miquel.auma@gmail.com>
2022-06-02 23:58:27 +0200

    chore: add data to DB at app startup

--- src/main/java/dev/auma/secure_api/SecureApiApplication.java
+++ src/main/java/dev/auma/secure_api/SecureApiApplication.java
@@ -1,13 +1,33 @@
 package dev.auma.secure_api;
 
+import dev.auma.secure_api.model.Transaction;
+import dev.auma.secure_api.service.TransactionService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @SpringBootApplication
 public class SecureApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecureApiApplication.class, args);
+	}
+
+	// Adds data to the database at application startup
+	@Bean
+	CommandLineRunner initializeData(TransactionService transactionService) {
+		return (args -> {
+			transactionService.saveNote(new Transaction(null, "transaction-01", "Some random text", new Date()));
+			transactionService.saveNote(new Transaction(null, "transaction-02", "Some random text", new Date()));
+			transactionService.saveNote(new Transaction(null, "transaction-03", "Some random text", new Date()));
+			transactionService.saveNote(new Transaction(null, "transaction-04", "Some random text", new Date()));
+
+			System.out.println(transactionService.getTransactions());
+		});
 	}
 
 }
