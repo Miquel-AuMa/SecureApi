e20f7d4bfb1bc76a0e3efa56233273c10ca32317
Miquel <miquel.auma@gmail.com>
2022-06-02 23:52:38 +0200

    feat: implement transaction controller

--- src/main/java/dev/auma/secure_api/controller/TransactionController.java
+++ src/main/java/dev/auma/secure_api/controller/TransactionController.java
@@ -1,4 +1,54 @@
 package dev.auma.secure_api.controller;
 
+import dev.auma.secure_api.exception.TransactionNotFoundException;
+import dev.auma.secure_api.model.Transaction;
+import dev.auma.secure_api.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transaction")
+@RequiredArgsConstructor
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    @GetMapping
+    public List<Transaction> findAll() {
+        return transactionService.getTransactions();
+    }
+
+    @GetMapping("/title/{title}")
+    public Transaction findByTitle(@PathVariable String name) throws TransactionNotFoundException {
+        return transactionService.getTransactionByName(name);
+    }
+
+    @GetMapping("/id/{id}")
+    public Transaction findById(@PathVariable Long id) throws TransactionNotFoundException{
+        return transactionService.getTransactionById(id);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Transaction> create(@RequestBody Transaction transaction) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/transaction/save").toUriString());
+        return ResponseEntity.created(uri).body(transactionService.saveNote(transaction));
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/update/{id}")
+    public void update(@PathVariable Long id, @RequestBody Transaction transaction) throws TransactionNotFoundException {
+        transactionService.updateTransaction(id, transaction);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id) {
+        transactionService.deleteTransaction(id);
+    }
+
 }
