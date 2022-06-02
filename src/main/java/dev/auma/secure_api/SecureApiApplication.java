0d59a1ddde9a35da1eca04ae69acb6a18a49a9d6
Miquel <miquel.auma@gmail.com>
2022-06-03 01:15:15 +0200

    chore: add users and roles creation into the DB

--- src/main/java/dev/auma/secure_api/SecureApiApplication.java
+++ src/main/java/dev/auma/secure_api/SecureApiApplication.java
@@ -1,13 +1,16 @@
 package dev.auma.secure_api;
 
+import dev.auma.secure_api.model.Role;
 import dev.auma.secure_api.model.Transaction;
+import dev.auma.secure_api.model.User;
 import dev.auma.secure_api.service.TransactionService;
+import dev.auma.secure_api.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 
 @SpringBootApplication
@@ -17,16 +20,30 @@ public class SecureApiApplication {
 		SpringApplication.run(SecureApiApplication.class, args);
 	}
 
-	// Adds data to the database at application startup
+	// Adds basic data to the database at application startup
 	@Bean
-	CommandLineRunner initializeData(TransactionService transactionService) {
+	CommandLineRunner initializeData(TransactionService transactionService, UserService userService) {
 		return (args -> {
+			// Creation of the transactions information
 			transactionService.saveNote(new Transaction(null, "transaction-01", "Some random text", new Date()));
 			transactionService.saveNote(new Transaction(null, "transaction-02", "Some random text", new Date()));
 			transactionService.saveNote(new Transaction(null, "transaction-03", "Some random text", new Date()));
 			transactionService.saveNote(new Transaction(null, "transaction-04", "Some random text", new Date()));
 
 			System.out.println(transactionService.getTransactions());
+
+			// Creation of the users and roles information
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+
+			userService.saveUser(new User(null, "User", "user", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Admin", "admin", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("user", "ROLE_USER");
+			userService.addRoleToUser("admin", "ROLE_USER");
+			userService.addRoleToUser("admin", "ROLE_ADMIN");
+
+			System.out.println(userService.getUsers());
 		});
 	}
 
